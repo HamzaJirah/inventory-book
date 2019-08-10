b8d5ede27b18613f5e2f6c0e5953f2c5f653ea15
HamzaJirah <hamzajirah@gmail.com>
2019-08-10 17:44:41 +0100

    created UI prototype to add item to UI

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 // Item constructor 
-function Item (item, qauntity, agent){
+function Item (item, quantity, agent){
   this.item = item;
-  this.qauntity = qauntity;
+  this.quantity = quantity;
   this.agent = agent;
 }
 
 // UI construtor 
 function UI(){}
+
+// add item to UI 
+UI.prototype.addItemToInventory = function(laptop) {
+  // get tbody from DOM
+  const tableBody = document.querySelector('#inventory-content');
+
+  // create row in the table 
+  const row = document.createElement('tr');
+
+  // insert laptop object in the UI 
+  row.innerHTML = `
+    <td>${laptop.item}</td>
+    <td>${laptop.quantity}</td>
+    <td>${laptop.agent}</td>
+    <td><i class="fas fa-trash"></i></td>
+  `
+
+  // append row to table body 
+  tableBody.appendChild(row);
+}
 
 // get form submission
 document.querySelector('#inventory-list').addEventListener('submit', e => {
@@ -23,7 +43,5 @@ document.querySelector('#inventory-list').addEventListener('submit', e => {
   // add item to UI 
   ui.addItemToInventory(laptop);
   
-
-
   e.preventDefault();
 });
