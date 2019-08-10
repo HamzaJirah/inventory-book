5705155c1d3fb1fd8a36e47f16e7f1968618b26a
HamzaJirah <hamzajirah@gmail.com>
2019-08-10 16:37:32 +0100

    instatiate Item and UI objects

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ function UI(){}
 
 // get form submission
 document.querySelector('#inventory-list').addEventListener('submit', e => {
+  const item = document.querySelector('#item').value;
+  const quantity = document.querySelector('#quantity').value;
+  const agent = document.querySelector('#agent').value;
+
+  // instantiate Item object
+  const laptop = new Item(item, quantity, agent);
+
+  // instantiate UI class
+  const ui = new UI();
+
+  // add item to UI 
+  ui.addItemToInventory(laptop);
+  
+
 
   e.preventDefault();
 });
